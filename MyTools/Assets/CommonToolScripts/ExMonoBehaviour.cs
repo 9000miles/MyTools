ede6a8428b40f13284f667f31642dbbf55e09ae3
chenjianfeng <chenjianfeng@hthyvr.com>
2018-04-09 10:58:56 +0800

    添加其他工具类

---
+++ MyTools/Assets/CommonToolScripts/ExMonoBehaviour.cs
@@ -0,0 +1,147 @@
+﻿//By:熊峻玉
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExMonoBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// 返回GameObject.name=name的 T 类型组件
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public T ExmChildComponent<T>(string name)
+    {
+        Transform tf = FindChild(this.transform, name);
+        if (tf == null)
+        {
+            return default(T);
+        }
+        return FindChild(this.transform, name).GetComponent<T>();
+    }
+
+    /// <summary>
+    /// 返回制定路径物体上的T类型组件
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="Path"></param>
+    /// <returns></returns>
+    public T ExmPathComponent<T>(string Path)
+    {
+        Transform tf = transform.FindChild(Path);
+        if (tf == null)
+        {
+            Debug.LogError("没有这个子物体: " + Path);
+            return default(T);
+        }
+        return tf.GetComponent<T>();
+    }
+
+    private static Transform fndChildResurt = null;
+
+    private static void FindChildRrecurrence(Transform trans, string goName)
+    {
+        for (int i = 0; i < trans.childCount; i++)
+        {
+            //如果容器有东西了就不做了
+            if (fndChildResurt != null)
+            {
+                return;
+            }
+            //符合条件就放到容器 并返回
+            if (trans.GetChild(i).name.Equals(goName))
+            {
+                Console.WriteLine(trans.GetChild(i));
+                fndChildResurt = trans.GetChild(i);
+                return;
+            }
+            //否则继续向下（深度优先）
+            FindChildRrecurrence(trans.GetChild(i), goName);
+        }
+    }
+
+    public static Transform FindChild(Transform trans, string goName)
+    {
+        ////先在直接下级找
+        //Transform child = trans.FindChild(goName);
+        ////如果找到返回
+        //if (child != null) return child;
+        ////没找到，继续在每一个子物体的下级找
+        //Transform go = null;
+        //for (int i = 0; i < trans.childCount; i++)
+        //{
+        //    child = trans.GetChild(i);
+        //    go = FindChild(child, goName);
+        //    if (go != null) return go;
+        //}
+        //return null;
+
+        //初始化容器
+        fndChildResurt = null;
+        //执行递归
+        FindChildRrecurrence(trans, goName);
+        //返回结果
+        return fndChildResurt;
+    }
+
+    #region 查找复合条件的所有子物体
+
+    //递归容器
+    protected List<Transform> list_TransformAllChild;
+
+    /// <summary>
+    /// 查找复合条件的所有子物体
+    /// </summary>
+    /// <param name="trans"></param>
+    /// <returns></returns>
+    public List<Transform> ExmFindAllChild(Func<Transform, bool> deligate)
+    {   //初始化容器
+        if (list_TransformAllChild == null) { list_TransformAllChild = new List<Transform>(); }
+        list_TransformAllChild.Clear();
+        //执行递归
+        recurrence(transform, list_TransformAllChild, deligate);
+        //返回结果
+
+        return list_TransformAllChild;
+    }
+
+    //递归体
+    private void recurrence(Transform trans, List<Transform> tempList, Func<Transform, bool> deligate)
+    {
+        //foreach (Transform item in trans)
+        //{
+        //    if (deligate(item))
+        //    {
+        //        tempList.Add(item);
+        //    }
+        //    recurrence(item, tempList, deligate);
+        //}
+
+        //   深度优先
+        for (int i = 0; i < trans.childCount; i++)
+        {
+            if (deligate(trans.GetChild(i)))
+            {
+                tempList.Add(trans.GetChild(i));
+            }
+            recurrence(trans.GetChild(i), tempList, deligate);
+        }
+
+        //广度优先
+        //for (int i = 0; i < trans.childCount; i++)
+        //{
+        //    if (deligate(trans.GetChild(i)))
+        //    {
+        //        tempList.Add(trans.GetChild(i));
+        //    }
+        //}
+        //for (int i = 0; i < trans.childCount; i++)
+        //{
+        //    recurrence(trans.GetChild(i), tempList, deligate);
+        //}
+    }
+
+    #endregion 查找复合条件的所有子物体
+}
---
+++ MyTools/Assets/CommonToolScripts/ListNodeBase.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+///选择逻辑节点父类(Unity) By:XiongJunYu
+///使用细节:
+///1子类使用前需要在子类中调用Add()添加元素
+///2添加当改变时的OnChange事件,OnChange事件中必须判断last不为null  !!!
+///3并使用StartSelect(currentIndex);设置一个初始选择
+///4重写Name()和SetActive()以明确对名称和设置激活的定义
+/// </summary>
+public abstract class ListNodeBase<T> : UnityEngine.MonoBehaviour
+{
+    public int currentIndex = 0;
+    public LogicLoopType loopType;
+
+    public bool otherAcitve = false;
+
+    public delegate void EventHandler(T last, T current);
+
+    public event EventHandler OnChangeEvent;
+
+    private int dirction = 1;
+    private T lastElement;
+
+    /// <summary>容器 </summary>
+    public List<T> contentList = new List<T>();
+
+    public T last { get { return lastElement; } }
+
+    public T current { get { return contentList[currentIndex]; } }
+
+    /// <summary>到上一个索引</summary>
+
+    public virtual void Last()
+    {
+        lastElement = current;
+
+        if (loopType == LogicLoopType.circle)
+        {
+            dirction = 1;
+            if (currentIndex == 0)
+            {
+                currentIndex = contentList.Count - 1;
+            }
+            else
+            {
+                currentIndex -= dirction;
+            }
+        }
+        else
+        {
+            if (loopType == LogicLoopType.pingpong)
+            {
+                currentIndex -= dirction;
+                if (currentIndex == -1 || currentIndex == contentList.Count)
+                {
+                    dirction *= -1;
+                    currentIndex -= 2 * dirction;
+                }
+            }
+        }
+
+        ChangeActive();
+        if (OnChangeEvent != null) OnChangeEvent(last, current);
+    }
+
+    /// <summary>到下一个索引 </summary>
+    public virtual void Next()
+    {
+        lastElement = current;
+
+        if (loopType == LogicLoopType.circle)
+        {
+            dirction = 1;
+            if (currentIndex == contentList.Count - 1)
+            {
+                currentIndex = 0;
+            }
+            else
+            {
+                currentIndex += dirction;
+            }
+        }
+        else
+        {
+            if (loopType == LogicLoopType.pingpong)
+            {
+                currentIndex += dirction;
+                if (currentIndex == -1 || currentIndex == contentList.Count)
+                {
+                    dirction *= -1;
+                    currentIndex += 2 * dirction;
+                }
+            }
+        }
+
+        ChangeActive();
+        if (OnChangeEvent != null) OnChangeEvent(last, current);
+    }
+
+    /// <summary>选择元素</summary>
+    public virtual void Select(int index)
+    {
+        if (index == currentIndex) return;
+        lastElement = current;
+
+        currentIndex = index;
+        ChangeActive();
+        if (OnChangeEvent != null) OnChangeEvent(last, current);
+    }
+
+    protected virtual void StartSelect(int index)
+    {
+        //if (index == currentIndex) return;
+        //lastElement = current;
+
+        currentIndex = index;
+        ChangeActive();
+        if (OnChangeEvent != null) OnChangeEvent(last, current);
+    }
+
+    public virtual void SelectByname(string s)
+    {
+        if (s == Name(current)) return;
+        int index = contentList.FindIndex(0, p => Name(p) == s);
+        Select(index);
+    }
+
+    /// <summary>获取名字</summary>
+    protected abstract string Name(T t);
+
+    /// <summary>当选择改变 </summary>
+
+    /// <summary> 设置为激活</summary>
+    protected abstract void SetActive(T t, bool active);
+
+    private void ChangeActive()
+    {
+        SetActive(current, true);
+        if (otherAcitve) return;
+
+        for (int i = 0; i < contentList.Count; i++)
+        {
+            if (i == currentIndex)
+            {
+                continue;
+            }
+            SetActive(contentList[i], false);
+        }
+    }
+
+    /// <summary>添加元素</summary>
+    public virtual void Add(T element)
+    {
+        contentList.Add(element);
+    }
+
+    public virtual void AddRange<Tenum>(Tenum elements) where Tenum : IEnumerable
+    {
+        foreach (T item in elements)
+        {
+            Add(item);
+        }
+    }
+
+    /// <summary> 移除元素</summary>
+    public virtual void Remove(int index)
+    {
+        contentList.RemoveAt(index);
+    }
+}
+
+[System.Serializable]
+public enum LogicLoopType
+{
+    circle,
+    pingpong
+}
