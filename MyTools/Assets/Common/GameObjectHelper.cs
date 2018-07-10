13468547e4bd52f9d961cf07a1dfc8d3c50060d4
lizijun <lizijun_youle@163.com>
2018-07-10 14:07:49 +0800

    新增选择物体脚本

---
+++ MyTools/Assets/Common/GameObjectHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectHelper
+{
+    /// <summary>
+    /// 获取该物体的路径
+    /// </summary>
+    /// <param name="go"></param>
+    /// <returns></returns>
+    public static string GetGameObjectPath(GameObject go)
+    {
+        if (go == null) return null;
+        string path = go.name;
+        while (go.transform.parent != null)
+        {
+            go = go.transform.parent.gameObject;
+            path = go.name + "/" + path;
+        }
+        return path;
+    }
+
+    /// <summary>
+    /// 获取选中的多个物体路径
+    /// </summary>
+    /// <param name="gos"></param>
+    /// <returns></returns>
+    public static string[] GetPaths(GameObject[] gos)
+    {
+        List<string> pathsList = new List<string>();
+        foreach (var item in gos)
+        {
+            if (item.transform.childCount > 0)
+            {
+                Transform[] tfs = item.GetComponentsInChildren<Transform>();
+                for (int i = 0; i < tfs.Length; i++)
+                {
+                    pathsList.Add(GetGameObjectPath(tfs[i].gameObject));
+                }
+            }
+            else
+                pathsList.Add(GetGameObjectPath(item));
+        }
+        return pathsList.ToArray();
+    }
+}
---
+++ MyTools/Assets/Editor/EditorWindow/SelectedGameObjectWindow.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SelectedGameObjectWindow : EditorWindow
+{
+    /// <summary> 存储的容量大小 /// </summary>
+    private static string count;
+
+    /// <summary> 选中的物体集合 </summary>
+    private static List<GameObject> objectList = new List<GameObject>();
+
+    /// <summary> 选中的物体 </summary>
+    private GameObject selecte;
+
+    private void OnGUI()
+    {
+        Window();
+    }
+
+    private void OnSelectionChange()
+    {
+        selecte = Selection.activeGameObject;
+        AddGameObject();
+    }
+
+    /// <summary>
+    /// 往集合中添加元素，如果没有则添加，如果数量满了则移除第一个
+    /// </summary>
+    /// <param name="go"></param>
+    private void AddGameObject()
+    {
+        if (selecte != null && objectList.Contains(selecte) == false)
+        {
+            int num = 0;
+            if (int.TryParse(count, out num))
+            {
+                if (objectList.Count >= num)
+                {
+                    objectList.RemoveAt(0);
+                }
+                objectList.Add(selecte);
+            }
+            else
+                Debug.LogError("-----  存储容量必须输入整数  -----");
+        }
+    }
+
+    /// <summary>
+    /// 选中前一个物体
+    /// </summary>
+    private void SelectedPreviouObject()
+    {
+        int index = objectList.FindIndex((t) => t == selecte);
+        if (index > 0)
+        {
+            selecte = objectList[index - 1];
+        }
+        Selection.activeGameObject = selecte;
+    }
+
+    /// <summary>
+    /// 选中后一个物体
+    /// </summary>
+    private void SelectedNextObject()
+    {
+        int index = objectList.FindIndex((t) => t == selecte);
+        if (index < objectList.Count - 1)
+        {
+            selecte = objectList[index + 1];
+        }
+        Selection.activeGameObject = selecte;
+    }
+
+    /// <summary>
+    /// 窗口显示图元
+    /// </summary>
+    private void Window()
+    {
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Space(8);
+        GUIStyle style = new GUIStyle();
+        style.fixedWidth = 55;
+        RectOffset offset = new RectOffset();
+        offset.top = 2;
+        style.padding = offset;
+        style.normal.background = null;
+        style.normal.textColor = new Color(0.7f, 0.7f, 0.7f);
+        GUILayout.Label("存储容量：", style);
+        count = GUILayout.TextField(count);
+        GUILayout.Space(5);
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("前一个物体"))
+        {
+            SelectedPreviouObject();
+        }
+        if (GUILayout.Button("后一个物体"))
+        {
+            SelectedNextObject();
+        }
+    }
+}
