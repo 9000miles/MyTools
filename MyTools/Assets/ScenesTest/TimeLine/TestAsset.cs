79b990985cce85e23a3319fdc2dfab5944a5921b
lizijun_home <1655753014@qq.com>
2018-07-29 20:15:11 +0800

    完善TimeLine，多行文本显示

--- MyTools/Assets/ScenesTest/TimeLine/TestAsset.cs
+++ MyTools/Assets/ScenesTest/TimeLine/TestAsset.cs
@@ -7,26 +7,13 @@ using UnityEngine.Timeline;
 
 public class TestAsset : PlayableAsset
 {
+    [SerializeField, TextArea(2, 5)]
     public string text = "";
-    private IEnumerable<PlayableBinding> playableBindings;
-    private TestBehavior testBehavior;
-    private Playable mPlayable;
-
-    public TestBehavior template = new TestBehavior();
-    public ExposedReference<Transform> listener;
 
     public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
     {
-        playableBindings = outputs;
-        //Debug.Log(duration);
-        //Debug.Log(owner.name);
-        mPlayable = Playable.Create(graph);
-        testBehavior = new TestBehavior();
-
-        var playable = ScriptPlayable<TestBehavior>.Create(graph, template);
+        ScriptPlayable<TestBehavior> playable = ScriptPlayable<TestBehavior>.Create(graph);
         TestBehavior clone = playable.GetBehaviour();
-        clone.listener = listener.Resolve(graph.GetResolver());
-        //TimelineAsset timelineAsset= testBehavior as TimelineAsset;
         return playable;
     }
 }
--- MyTools/Assets/ScenesTest/TimeLine/TestBehavior.cs
+++ MyTools/Assets/ScenesTest/TimeLine/TestBehavior.cs
@@ -7,35 +7,34 @@ using UnityEngine.Timeline;
 
 public class TestBehavior : PlayableBehaviour
 {
-    public Transform listener;
     private PlayableDirector playableDirector;
 
     public override void OnBehaviourDelay(Playable playable, FrameData info)
     {
         base.OnBehaviourDelay(playable, info);
-        //Debug.Log("OnBehaviourDelay");
+        Debug.Log("OnBehaviourDelay");
     }
 
-    public override void OnBehaviourPause(Playable playable, FrameData info)
+    public override void OnBehaviourPause(Playable playable, FrameData info)//每个片段结束播放，就会调用该函数
     {
         base.OnBehaviourPause(playable, info);
-        Debug.Log("OnBehaviourPause");
+        //Debug.Log("OnBehaviourPause");
     }
 
-    public override void OnBehaviourPlay(Playable playable, FrameData info)
+    public override void OnBehaviourPlay(Playable playable, FrameData info)//每个片段开始播放，就会调用该函数
     {
         base.OnBehaviourPlay(playable, info);
         playableDirector = GameObject.Find("TimeLine").GetComponent<PlayableDirector>();
-        Debug.Log("OnBehaviourPlay");
+        //Debug.Log("OnBehaviourPlay");
     }
 
-    public override void OnGraphStart(Playable playable)
+    public override void OnGraphStart(Playable playable)//TimeLine编辑之后，会调用
     {
         base.OnGraphStart(playable);
         //Debug.Log("OnGraphStart");
     }
 
-    public override void OnGraphStop(Playable playable)
+    public override void OnGraphStop(Playable playable)//TimeLine编辑之后，会调用
     {
         base.OnGraphStop(playable);
         //Debug.Log("OnGraphStop");
@@ -44,18 +43,18 @@ public class TestBehavior : PlayableBehaviour
     public override void PrepareData(Playable playable, FrameData info)
     {
         base.PrepareData(playable, info);
-        //Debug.Log("PrepareData");
+        Debug.Log("PrepareData");
     }
 
-    public override void PrepareFrame(Playable playable, FrameData info)
+    public override void PrepareFrame(Playable playable, FrameData info)//每帧调用
     {
         base.PrepareFrame(playable, info);
-        //Debug.Log("PrepareFrame");
+        //Debug.Log("PrepareFrame" + "            " + playableDirector.time);
     }
 
-    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)//每帧调用
     {
         base.ProcessFrame(playable, info, playerData);
-        Debug.Log("ProcessFrame" + "            " + playableDirector.time);
+        //Debug.Log("ProcessFrame" + "            " + playableDirector.time);
     }
 }
