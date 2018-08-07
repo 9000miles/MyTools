9557f1fcca7273e7f3cf8f4ff0b5cc9aaeacf855
lizijun <lizijun_youle@163.com>
2018-08-07 19:39:23 +0800

    添加根据枚举值显示不同的内容

---
+++ MyTools/Assets/Editor/InspectorWindow/DisplayerEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Displayer))]
+public class DisplayerEditor : Editor
+{
+    private Displayer obj;
+    private void Awake()
+    {
+        obj = target as Displayer;
+        if (obj.instanceA == null)
+            obj.instanceA = new A();
+        if (obj.instanceB == null)
+            obj.instanceB = new B(); 
+    }
+     
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (obj.type == TestType.A)
+        {
+            EditorGUILayout.LabelField("A对象");
+            obj.instanceA.a = EditorGUILayout.TagField(obj.instanceA.a);
+        }
+        else
+        {
+            EditorGUILayout.LabelField("B对象");
+            obj.instanceB.b = EditorGUILayout.TextField(obj.instanceB.b);
+        }
+    }
+}
---
+++ MyTools/Assets/ScenesTest/MouseGestures/PlanE/Displayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TestType
+{
+   A,
+   B
+}
+public class Displayer : MonoBehaviour
+{
+    public TestType type;
+
+    public A instanceA;
+
+    public B instanceB;
+	 
+}
+
+public class A
+{
+    public string a;
+}
+
+public class B
+{
+    public string b;
+}
