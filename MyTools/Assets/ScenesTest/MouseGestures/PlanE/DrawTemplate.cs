f70b11a952cd09b6d02da1b9be5b438e5eaad379
lizijun <lizijun_youle@163.com>
2018-08-07 16:52:34 +0800

    PlanE计划，夹角线条的交点计算完毕，需要剔除不在线段内的点

--- MyTools/Assets/ScenesTest/MouseGestures/PlanE/DrawTemplate.cs
+++ MyTools/Assets/ScenesTest/MouseGestures/PlanE/DrawTemplate.cs
@@ -7,8 +7,13 @@ using Common;
 //[RequireComponent(typeof(BezierGenerate))]
 public class DrawTemplate : MonoBehaviour
 {
+    private bool isDraw;
     private Transform pointsPanel;
+    public Transform crossPoint;
+    public Transform crossPointPanel;
     private List<Transform> allPoint;
+    private List<AngleLine> angleLines;
+    private List<Vector2> crossingPoint;
     private LineRenderer lineRenderer;
     private BezierGenerate bezier;
     public float length = 400;
@@ -22,6 +27,8 @@ public class DrawTemplate : MonoBehaviour
     {
         pointsPanel = transform.Find("PointsPanel");
         allPoint = new List<Transform>();
+        angleLines = new List<AngleLine>();
+        crossingPoint = new List<Vector2>();
         lineRenderer = GetComponent<LineRenderer>();
         bezier = GetComponent<BezierGenerate>();
         Init();
@@ -44,10 +51,12 @@ public class DrawTemplate : MonoBehaviour
     private void Update()
     {
         DrawAngleLine();
+        DrawAngleLineCoordinates();
     }
 
     private void DrawAngleLine()
     {
+        angleLines.Clear();
         lineRenderer.positionCount = allPoint.Count;
         lineRenderer.SetPositions(allPoint.ToArray().Select(t => t.position));
 
@@ -72,6 +81,9 @@ public class DrawTemplate : MonoBehaviour
             angleOffset = Quaternion.Euler(new Vector3(0, 0, -angle)) * dir;
             Vector3 end = allPoint[i].position + angleOffset.normalized * length;
 
+            angleLines.Add(new AngleLine(allPoint[i].position, start));
+            angleLines.Add(new AngleLine(allPoint[i].position, end));
+
             angleLine.SetPosition(0, start);
             angleLine.SetPosition(1, allPoint[i].position);
             angleLine.SetPosition(2, end);
@@ -91,6 +103,9 @@ public class DrawTemplate : MonoBehaviour
                 angleOffset = Quaternion.Euler(new Vector3(0, 0, -angle)) * dir;
                 Vector3 end = allPoint[i].position + angleOffset.normalized * length;
 
+                angleLines.Add(new AngleLine(allPoint[i].position, start));
+                angleLines.Add(new AngleLine(allPoint[i].position, end));
+
                 angleLine.SetPosition(0, start);
                 angleLine.SetPosition(1, allPoint[i].position);
                 angleLine.SetPosition(2, end);
@@ -106,10 +121,68 @@ public class DrawTemplate : MonoBehaviour
                 angleOffsetNext = Quaternion.Euler(new Vector3(0, 0, -angle)) * dirNext;
                 Vector3 endNext = allPoint[i].position + angleOffsetNext.normalized * length;
 
+                angleLines.Add(new AngleLine(allPoint[i].position, startNext));
+                angleLines.Add(new AngleLine(allPoint[i].position, endNext));
+
                 angleLine.SetPosition(4, startNext);
                 angleLine.SetPosition(5, allPoint[i].position);
                 angleLine.SetPosition(6, endNext);
             }
         }
+    }
+
+    private void DrawAngleLineCoordinates()
+    {
+        if (isDraw == true) return;
+        foreach (var item in angleLines)
+        {
+            foreach (var inItem in angleLines)
+            {
+                if (item != inItem)
+                {
+                    Vector2 crossPoint = CalculateIntersectionCoordinates(item.start, item.end, inItem.start, inItem.end);
+                    if (!crossingPoint.Contains(crossPoint))
+                    {
+                        crossingPoint.Add(crossPoint);
+                    }
+                }
+            }
+        }
+        foreach (var item in crossingPoint)
+        {
+            Instantiate(crossPoint, item, Quaternion.identity, crossPointPanel);
+        }
+        isDraw = true;
+    }
+
+    /// <summary>
+    /// 计算交点坐标
+    /// </summary>
+    private Vector2 CalculateIntersectionCoordinates(Vector2 P1, Vector2 P2, Vector2 P3, Vector2 P4)
+    {
+        Vector2 result = new Vector2();
+        float left, right;
+        left = (P2.y - P1.y) * (P4.x - P3.x) - (P4.y - P3.y) * (P2.x - P1.x);
+        right = (P3.y - P1.y) * (P2.x - P1.x) * (P4.x - P3.x) + (P2.y - P1.y) * (P4.x - P3.x) * P1.x - (P4.y - P3.y) * (P2.x - P1.x) * P3.x;
+        result.x = (int)(right / left);
+        left = (P2.x - P1.x) * (P4.y - P3.y) - (P4.x - P3.x) * (P2.y - P1.y);
+        right = (P3.x - P1.x) * (P2.y - P1.y) * (P4.y - P3.y) + P1.y * (P2.x - P1.x) * (P4.y - P3.y) - P3.y * (P4.x - P3.x) * (P2.y - P1.y);
+        result.y = (int)(right / left);
+        return result;
+    }
+}
+
+/// <summary>
+/// 角度线条
+/// </summary>
+public class AngleLine
+{
+    public Vector2 start;
+    public Vector2 end;
+
+    public AngleLine(Vector2 start, Vector2 end)
+    {
+        this.start = start;
+        this.end = end;
     }
 }
