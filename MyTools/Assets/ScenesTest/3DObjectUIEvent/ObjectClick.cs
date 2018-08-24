70781576db0ab53613155af2870a5842f8fb42e6
lizijun <lizijun_youle@163.com>
2018-08-24 18:31:24 +0800

    添加3D物体响应UI事件功能

---
+++ MyTools/Assets/ScenesTest/3DObjectUIEvent/ObjectClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObjectClick : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("pointerPress            " + eventData.pointerPress);
+        //Debug.Log("pointerDrag            " + eventData.pointerDrag);
+        //Debug.Log("rawPointerPress            " + eventData.rawPointerPress);
+        //Debug.Log("lastPress            " + eventData.lastPress);
+        //Debug.Log("pointerEnter            " + eventData.pointerEnter);
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    GameObject go = OnePointColliderObject();
+        //    if (go != null)
+        //        Debug.Log("                  ---------------" + go.name);
+        //}
+    }
+
+    /// <summary>
+    /// 获取鼠标点击的UI对象
+    /// </summary>
+    /// <returns>点击的UI对象</returns>
+    public GameObject OnePointColliderObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0 ? results[0].gameObject : null;
+    }
+}
