1156ff20d6a3d2352d213bd10a44d2eacae778c3
lizijun <1655753014@qq.com>
2018-11-01 18:26:32 +0800

    导入第三人称插件，有布娃娃系统，攀爬障碍，爬梯子

---
+++ MyTools/Assets/ScriptsTest/DeadTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion.FinalIK;
+
+public class DeadTest : MonoBehaviour
+{
+    RagdollUtility ragdollUtility;
+    bool ifDead;
+    // Use this for initialization
+    void Start()
+    {
+        ragdollUtility = GetComponent<RagdollUtility>();
+        ifDead = false;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.O))
+        {
+            if (ifDead == false)
+            {
+                ragdollUtility.EnableRagdoll();
+            }
+            else
+            {
+                ragdollUtility.DisableRagdoll();
+            }
+            ifDead = !ifDead;
+        }
+    }
+}
