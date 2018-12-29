685e03c1bc5804101ea334b323e7ea951bebf432
lizijun <1655753014@qq.com>
2018-12-29 20:33:55 +0800

    add

---
+++ MyTools/Assets/ScenesTest/EnumTest/EnumTest.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumTest : MonoBehaviour
+{
+    //[EnumFlags]
+    public EEHHH eHHH;
+    [Header("First")]
+    public int age;
+    public int owljo;
+    [SerializeField]
+    private int oaoejojw;
+
+    public void Start()
+    {
+        int value = (int)eHHH;
+        Debug.Log("枚举选中的值  " + value);
+    }
+}
+
+public enum EEHHH
+{
+    None = 0,
+    oweio = 6,
+    jofoiwejfo = 1,
+    jofw = 2,
+    jofwoe = 3,
+    owej = 4,
+    owoe = 5
+}
