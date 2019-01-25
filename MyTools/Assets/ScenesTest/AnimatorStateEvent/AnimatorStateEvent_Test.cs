2214467adc5022af7fd3969a7cdd95fab0b9e6ef
lizijun <lizijun@hthygame.com>
2019-01-25 18:09:01 +0800

    更新Animator动画事件

--- MyTools/Assets/ScenesTest/AnimatorStateEvent/AnimatorStateEvent_Test.cs
+++ MyTools/Assets/ScenesTest/AnimatorStateEvent/AnimatorStateEvent_Test.cs
@@ -23,13 +23,14 @@ namespace MarsPC
 
         private void AnimatorStateEvent_TestFunction()
         {
-            //---------------------------  animator_00  ----------------------------
+            //---------------------------  animator_00  -------------------------------------------------------  animator_00  ----------------------------
+            //---------------------------  animator_00  -------------------------------------------------------  animator_00  ----------------------------
+            //---------------------------  animator_00  -------------------------------------------------------  animator_00  ----------------------------
             action = () => { Debug.Log("animator_00 -- OnEnterCall_00------------------------------"); };
             AnimatorStateEvent stateEvent00_00 = new AnimatorStateEvent()
             {
-                fullName = "Base Layer.RunFwd",
-                layerIndex = 0,
-                delayTime = 0.4f,
+                FullName = "BaseLayer.Climb_Wall_State.Wall_Climb_Idle",
+                DelayTime = 0.4f,
                 OnEnterCall = action,
                 OnUpdateCall = () => { Debug.Log("animator_00 -- OnUpdateCall_00"); },
                 OnDelayCall = () => { Debug.Log("animator_00 -- OnDelayCall_00     --  " + Time.time); },
@@ -38,26 +39,23 @@ namespace MarsPC
             AnimatorStateEventManager.Singleton.AddStateEvent(animator_00, stateEvent00_00);
             AnimatorStateEvent stateEvent00_10 = new AnimatorStateEvent()
             {
-                fullName = "Base Layer.RunFwd",
-                layerIndex = 0,
-                delayTime = 0.6f,
+                FullName = "Base Layer.RunFwd",
+                DelayTime = 0.6f,
                 OnDelayCall = () => { Debug.Log("animator_00 -- OnDelayCall_00     --  " + Time.time); },
             };
             AnimatorStateEventManager.Singleton.AddStateEvent(animator_00, stateEvent00_10);
             AnimatorStateEvent stateEvent00_11 = new AnimatorStateEvent()
             {
-                fullName = "Base Layer.RunFwd",
-                layerIndex = 0,
-                delayTime = 0.8f,
+                FullName = "Base Layer.RunFwd",
+                DelayTime = 0.8f,
                 OnDelayCall = () => { Debug.Log("animator_00 -- OnDelayCall_00     --  " + Time.time); },
             };
             AnimatorStateEventManager.Singleton.AddStateEvent(animator_00, stateEvent00_11);
 
             AnimatorStateEvent stateEvent00_01 = new AnimatorStateEvent()
             {
-                fullName = "New Layer.RunLeft",
-                layerIndex = 1,
-                delayTime = 0.6f,
+                FullName = "New Layer.RunLeft",
+                DelayTime = 0.6f,
                 OnEnterCall = () => { Debug.Log("animator_00 -- OnEnterCall_01"); },
                 OnUpdateCall = () => { Debug.Log("animator_00 -- OnUpdateCall_01"); },
                 OnDelayCall = () => { Debug.Log("animator_00 -- OnDelayCall_01"); },
@@ -65,12 +63,24 @@ namespace MarsPC
             };
             AnimatorStateEventManager.Singleton.AddStateEvent(animator_00, stateEvent00_01);
 
-            //---------------------------  animator_01  ----------------------------
+            string[] setIsMovingState = {
+                "BaseLayer.Ledge_Hang_State.Stand_ToLedgeHang_Dn",
+                "BaseLayer.Ledge_Hang_State.Ledge_Hang_Shimmy_Lf",
+                "BaseLayer.Ledge_Hang_State.Ledge_Hang_Shimmy_Rt",
+            };
+            AnimatorStateEvent stateEvent_02 = new AnimatorStateEvent()
+            {
+                OnExitCall = () => { Debug.Log("animator_00 -- 状态全名不一样的同类事件"); },
+            };
+            AnimatorStateEventManager.Singleton.AddStateEvent(animator_00, setIsMovingState, stateEvent_02);
+
+            //---------------------------  animator_01  -------------------------------------------------------  animator_01  ----------------------------
+            //---------------------------  animator_01  -------------------------------------------------------  animator_01  ----------------------------
+            //---------------------------  animator_01  -------------------------------------------------------  animator_01  ----------------------------
             AnimatorStateEvent stateEvent01_00 = new AnimatorStateEvent()
             {
-                fullName = "Base Layer.RunFwdStart",
-                layerIndex = 0,
-                delayTime = 0.4f,
+                FullName = "Base Layer.RunFwdStart",
+                DelayTime = 0.4f,
                 OnEnterCall = () => { Debug.Log("animator_01 -- OnEnterCall_00"); },
                 OnUpdateCall = () => { Debug.Log("animator_01 -- OnUpdateCall_00"); },
                 OnDelayCall = () => { Debug.Log("animator_01 -- OnDelayCall_00"); },
@@ -80,9 +90,8 @@ namespace MarsPC
 
             AnimatorStateEvent stateEvent01_01 = new AnimatorStateEvent()
             {
-                fullName = "Base Layer.New StateMachine.RunLt135",
-                layerIndex = 0,
-                delayTime = 0.2f,
+                FullName = "Base Layer.New StateMachine.RunLt135",
+                DelayTime = 0.2f,
                 OnEnterCall = () => { Debug.Log("animator_01 -- OnEnterCall_00"); },
                 OnUpdateCall = () => { Debug.Log("animator_01 -- OnUpdateCall_00"); },
                 OnDelayCall = () => { Debug.Log("animator_01 -- OnDelayCall_00"); },
@@ -123,8 +132,7 @@ namespace MarsPC
             {
                 AnimatorStateEvent stateEvent00_00 = new AnimatorStateEvent()
                 {
-                    fullName = "Base Layer.RunFwd",
-                    layerIndex = 0,
+                    FullName = "Base Layer.RunFwd",
                     OnEnterCall = () => { Debug.Log("animator_00 -- OnEnterCall_00 [ Add ]"); },
                 };
                 AnimatorStateEventManager.Singleton.AddStateEvent(animator_00, stateEvent00_00);
